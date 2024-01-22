e9b386f0347b20b945bf9507efc4c41dee74adc1
luclaurent <luc.laurent@lecnam.net>
2024-01-22 14:45:41 +0100

    add new files

---
+++ json2hal.py
@@ -0,0 +1,240 @@
+from lxml import etree
+import re
+import logging
+
+FORMAT = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
+logging.basicConfig(format=FORMAT)
+Logger = logging.getLogger('json2hal')
+
+
+ID_ORCID_URL='http://orcid.org/''
+ID_ARXIV_URL='http://arxiv.org/a/'
+ID_RESEARCHERID_URL='http://www.researcherid.com/rid/'
+ID_IDREF_URL='http://www.idref.fr/'
+ID_CC_URL='https://creativecommons.org/licenses/'
+
+DEFAULT_AUDIENCE='1'
+DEFAULT_INVITED='0'
+DEFAULT_POPULAR='0'
+DEFAULT_PEER='0'
+DEFAULT_PROCEEDINGS='0'
+
+def setTitles(nInTree,titles):
+    nTitle = list()
+    if titles == dict:
+        for l,t in titles.items():
+            nTitle.append(etree.SubElement(nInTree, "title"))
+            nTitle[-1].set("xml:lang",l)
+            nTitle[-1].text = t
+    else:
+        Logger.warning("No language for title: force english")
+        nTitle.append(etree.SubElement(nInTree, "title"))
+        nTitle[-1].set("xml:lang","en")
+        nTitle[-1].text = titles
+    return nTitle
+    
+def setSubTitles(nInTree,titles):
+    nTitle = list()
+    if titles == dict:
+        for l,t in titles.items():
+            nTitle.append(etree.SubElement(nInTree, "title"))
+            nTitle[-1].set("type","sub")
+            nTitle[-1].set("xml:lang",l)
+            nTitle[-1].text = t
+    else:
+        Logger.warning("No language for title: force english")
+        nTitle.append(etree.SubElement(nInTree, "title"))
+        nTitle[-1].set("type","sub")
+        nTitle[-1].set("xml:lang","en")
+        nTitle[-1].text = titles
+    return nTitle
+
+def setAuthors(inTree,authors):
+    nAuthors = list()
+    for a in authors:
+        nAuthors.append(etree.SubElement(inTree, "author"))
+        # roles: https://api-preprod.archives-ouvertes.fr/ref/metadataList/?q=metaName_s:relator&fl=*&wt=xml
+        if "role" in a:
+            nAuthors[-1].set("role",a["role"])
+        else:
+            Logger.warning("No role for author {} {}: force aut".format(a["name"][0],a["name"][-1]))
+            nAuthors[-1].set("role","aut")
+        persName = etree.SubElement(nAuthors[-1], "persName")
+        forename = etree.SubElement(persName, "forename")
+        forename.set("type","first")
+        forename.text = a["name"][0]
+        if len(a["name"])>2:
+            forename = etree.SubElement(persName, "forename")
+            forename.set("type","middle")
+            forename.text = a["name"][1]
+        surename = etree.SubElement(persName, "surename")
+        surename.text = a[-1]
+        if "email" in a:
+            idA = etree.SubElement(nAuthors[-1], "email")
+            idA.text = a["email"]
+        if "idhal" in a:
+            idA = etree.SubElement(nAuthors[-1], "idno")
+            idA.set("type","idhal")
+            idA.text = a["idhal"]
+        if "halauthor" in a:
+            idA = etree.SubElement(nAuthors[-1], "idno")
+            idA.set("type","halauthor")
+            idA.text = a["halauthor"]
+        if "url" in a:
+            idA = etree.SubElement(nAuthors[-1], "ptr")
+            idA.set("type","url")
+            idA.set("target",a["url"])
+        if "orcid" in a:
+            idA = etree.SubElement(nAuthors[-1], "idno")
+            idA.set("type",ID_ORCID_URL)
+            idA.text = a["orcid"]
+        if "arxiv" in a:
+            idA = etree.SubElement(nAuthors[-1], "idno")
+            idA.set("type",ID_ARXIV_URL)
+            idA.text = a["arxiv"]
+        if "researcherid" in a:
+            idA = etree.SubElement(nAuthors[-1], "idno")
+            idA.set("type",ID_RESEARCHERID_URL)
+            idA.text = a["researcherid"]
+        if "idref" in a:
+            idA = etree.SubElement(nAuthors[-1], "idno")
+            idA.set("type",ID_IDREF_URL)
+            idA.text = a["idref"]
+        if "affiliation" in a:
+            if type(a["affiliation"]) is not list:
+                list_aff = [a["affiliation"]]
+            else:
+                list_aff = a["affiliation"]
+            for aff in list_aff:
+                nAff = etree.SubElement(nAuthors[-1], "affiliation")
+                if 'structHal' in aff:
+                    idStrut = aff['structHal']
+                    idStruct = re.sub('^#struct-','',idStrut)
+                    nAff.set("ref",'#struct-'+idStruct)
+                elif 'locAff' in aff:
+                    nAff.set("ref",'#localStruct-'+aff['locAff'])
+    return nAuthors
+                
+def setLicense(inTree,license):
+    availability = etree.SubElement(inTree, "availability")
+    lic_cc = etree.SubElement(availability, "license")
+    # all licenses: https://api-preprod.archives-ouvertes.fr/ref/metadataList/?q=metaName_s:licence&fl=*&wt=xml
+    lic_cc.set('target',ID_CC_URL+'/'+license["version"]+'/')
+    
+def setStamps(inTree,stamps):
+    nStamps = list()
+    if type(stamps) is not list:
+        list_stamps = [stamps]
+    else:
+        list_stamps = stamps
+    for s in list_stamps:
+        nStamps.append(etree.SubElement(inTree, "idno"))
+        nStamps[-1].set("type","stamp")
+        nStamps[-1].set("n",s["name"])
+    return nStamps
+
+def getAudience(notes):
+    # see all audience codes: https://api-preprod.archives-ouvertes.fr/ref/metadataList/?q=metaName_s:audience&fl=*&wt=xml
+    # default audience
+    audienceFlag = notes.get('audience',DEFAULT_AUDIENCE)
+    if str(audienceFlag).lower() == 'international':
+        audienceFlag = '2'
+    elif str(audienceFlag).lower() == 'national':
+        audienceFlag = '3'
+    if str(audienceFlag) != '1' and str(audienceFlag) != '2' and str(audienceFlag) != '3':
+        Logger.warning("Unknown audience: force default")
+        audienceFlag = DEFAULT_AUDIENCE
+    return audienceFlag
+
+def getInvited(notes):
+    # see all invited codes: https://api-preprod.archives-ouvertes.fr/ref/metadataList/?q=metaName_s:invitedCommunication&fl=*&wt=xml
+    # default invited
+    invitedFlag = notes.get('invited',DEFAULT_INVITED)
+    if str(invitedFlag).lower() == 'oui':
+        invitedFlag = '1'
+    elif str(invitedFlag).lower() == 'non':
+        invitedFlag = '0'
+    if str(invitedFlag) != '0' and str(invitedFlag) != '1':
+        Logger.warning("Unknown invited: force default")
+        invitedFlag = DEFAULT_INVITED
+    return invitedFlag
+
+def getPopular(notes):
+    # see all popular levels codes: hhttps://api-preprod.archives-ouvertes.fr/ref/metadataList/?q=metaName_s:popularLevel&fl=*&wt=xml
+    # default pouplar level
+    popFlag = notes.get('invited',DEFAULT_POPULAR)
+    if str(popFlag).lower() == 'oui':
+        popFlag = '1'
+    elif str(popFlag).lower() == 'non':
+        popFlag = '0'
+    if str(popFlag) != '0' and str(popFlag) != '1':
+        Logger.warning("Unknown popular level: force default")
+        popFlag = DEFAULT_POPULAR
+    return popFlag
+
+def getPeer(notes):
+    # see all peer reviewing codes: hhttps://api-preprod.archives-ouvertes.fr/ref/metadataList/?q=metaName_s:popularLevel&fl=*&wt=xml
+    # default peer reviewing
+    peerFlag = notes.get('invited',DEFAULT_PEER)
+    if str(peerFlag).lower() == 'oui':
+        peerFlag = '1'
+    elif str(peerFlag).lower() == 'non':
+        peerFlag = '0'
+    if str(peerFlag) != '0' and str(peerFlag) != '1':
+        Logger.warning("Unknown peer reviewing type: force default")
+        peerFlag = DEFAULT_PEER
+    return peerFlag
+
+def getProceedings(notes):
+    # see all peer reviewing codes: https://api-preprod.archives-ouvertes.fr/ref/metadataList/?q=metaName_s:proceedings&fl=*&wt=xml
+    # default proceedings
+    proFlag = notes.get('invited',DEFAULT_PROCEEDINGS)
+    if str(proFlag).lower() == 'oui':
+        proFlag = '1'
+    elif str(proFlag).lower() == 'non':
+        proFlag = '0'
+    if str(proFlag) != '0' and str(proFlag) != '1':
+        Logger.warning("Unknown proceedings status: force default")
+        proFlag = DEFAULT_PROCEEDINGS
+    return proFlag
+
+def setNotes(inTree,notes):
+    nNotes = list()
+    if type(notes) is not list:
+        list_notes = [notes]
+    else:
+        list_notes = notes
+    # define audience
+    nNotes.append(etree.SubElement(inTree, "note"))
+    nNotes[-1].set("type","audience")
+    nNotes[-1].set("n",getAudience(list_notes))
+    # define invited
+    nNotes.append(etree.SubElement(inTree, "note"))
+    nNotes[-1].set("type","invited")
+    nNotes[-1].set("n",getInvited(list_notes))
+    # define popular
+    nNotes.append(etree.SubElement(inTree, "note"))
+    nNotes[-1].set("type","popular")
+    nNotes[-1].set("n",getPopular(list_notes))
+    # define peer
+    nNotes.append(etree.SubElement(inTree, "note"))
+    nNotes[-1].set("type","peer")
+    nNotes[-1].set("n",getPeer(list_notes))
+    # define proceedings
+    nNotes.append(etree.SubElement(inTree, "note"))
+    nNotes[-1].set("type","proceedings")
+    nNotes[-1].set("n",getProceedings(list_notes))
+    
+    for n in list_notes:
+        if "comment" in n:
+            nNotes.append(etree.SubElement(inTree, "note"))
+            nNotes[-1].set("type","commentary")
+            nNotes[-1].text = n["comment"]
+        if "description" in n:
+            nNotes.append(etree.SubElement(inTree, "note"))
+            nNotes[-1].set("type","description")
+            nNotes[-1].text = n["description"]
+            
+    return nNotes
+
+def setAbstract(inTree,abstract):
---
+++ pdf2hal.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python
+
+import sys,os,shutil
+import json
+import tempfile
+import argparse
+from pdftitle import get_title_from_file as titleFromPdf
+import requests
+from requests.auth import HTTPBasicAuth
+# from sword2 import Connection
+import logging
+import fitz
+from lxml import etree
+
+FORMAT = 'PDF2HAL - %(asctime)s -  %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+Logger = logging.getLogger('pdf2hal')
+
+DEFAULT_NB_CHAR=400
+DEFAULT_CREDENTIALS_FILE='.apihal'
+DEFAULT_UPLOAD_FILE_NAME_PDF='{}.pdf' #'upload.pdf'
+DEFAULT_UPLOAD_FILE_NAME_XML='upload.xml'
+DEFAULT_UPLOAD_FILE_NAME_ZIP='upload'
+TXT_SEP='++++++++++++++++++++++'
+DEFAULT_NAMESPACE_XML={'tei': 'http://www.tei-c.org/ns/1.0'}
+ARCHIVES_API_URL = "https://api.archives-ouvertes.fr/search/"
+ARCHIVES_TEI_URL = "https://api.archives-ouvertes.fr/oai/TEI/{hal_id}"
+ARCHIVES_SWORD_API_URL = "https://api.archives-ouvertes.fr/sword/hal/" 
+ARCHIVES_SWORD_PRE_API_URL = "https://api-preprod.archives-ouvertes.fr/sword/hal/"
+
+def showPDFcontent(pdf_path, number=DEFAULT_NB_CHAR):
+    try:
+        doc = fitz.open(pdf_path)
+        text = ""
+
+        for page_num in range(doc.page_count):
+            page = doc[page_num]
+            text += page.get_text()
+            if len(text)>number:
+                break
+
+        # Display the first nb characters
+        Logger.info('Content of file: {}'.format(pdf_path))
+        Logger.info(TXT_SEP)
+        for line in text[:number].split('\n'):
+            Logger.info(line)
+        Logger.info(TXT_SEP)
+    except Exception as e:
+        Logger.error(f"Error: {e}")
+
+def load_credentials(args):
+    cred = dict()
+    if args.login and args.passwd:
+        Logger.debug('Load credentials from arguments')
+        cred['login']=args.login
+        cred['passwd']=args.passwd
+    else:
+        Logger.debug('Load credentials from file')
+        if os.path.isfile(DEFAULT_CREDENTIALS_FILE):
+            with open(DEFAULT_CREDENTIALS_FILE) as f:
+                cred = json.load(f)
+    
+    return cred
+
+def search_title_in_archives(pdf_title):
+    Logger.debug(f"Searching for title: {pdf_title}")
+    params = {
+        'q': f'title_t:"{pdf_title}"',
+        'fl': 'title_s,author_s,halId_s,label_s,docid',
+        'wt': 'json',
+        'rows': 5,  # Adjust the number of rows based on your preference
+    }
+
+    response = requests.get(ARCHIVES_API_URL, params=params)
+
+    if response.status_code == 200:
+        data = response.json().get('response', {}).get('docs', [])
+        return data
+
+    return []
+
+def choose_from_results(results, forceSelection=False):
+    Logger.info(TXT_SEP)
+    Logger.info("Multiple results found:")
+    for i, result in enumerate(results):
+        Logger.info("[{}/{}]. {} - {}".format(i + 1,len(results),result.get('label_s', 'N/A'),result.get('halId_s', 'N/A')))
+
+    Logger.info("Select a number to view details (0 to skip or m for manual definition): ")
+    if forceSelection:
+        choice = '1'
+    else:
+        choice = input(' > ')
+
+    if choice.isdigit() and 0 <= int(choice) <= len(results):
+        selected_result = results[int(choice) - 1]
+        return selected_result
+    elif choice == 'm':
+        Logger.info('Provide title manually')
+        manualTitle = input(' > ')
+        return manualTitle
+    else:
+        Logger.warning("Invalid choice. Skipping.")
+
+    return None
+
+def download_tei_file(hal_id):
+    Logger.debug(f"Downloading TEI file for halId: {hal_id}")
+    params = {
+        'q': f'halId_s:"{hal_id}"',
+        'wt': 'xml-tei'
+    }
+
+    response = requests.get(ARCHIVES_API_URL, params=params)
+
+    if response.status_code == 200:
+        data = response.text
+        #declare namespace
+        key, value = list(DEFAULT_NAMESPACE_XML.items())[0]
+        etree.register_namespace(key, value)
+        return etree.fromstring(data.encode('utf-8'))
+
+    return []
+
+def writeXML(inTree,file_path):
+    Logger.debug('Write XML file: {}'.format(file_path))
+    et = etree.ElementTree(inTree)
+    et.write(file_path)
+        # f.write(etree.tostring(inTree, pretty_print=True, xml_declaration=True, encoding='utf-8'))
+
+def cleanXML(inTree):
+    # remove stamps
+    for bad in inTree.findall(".//idno[@type='stamp']",inTree.nsmap):
+        bad.getparent().remove(bad)
+
+
+def addFileInXML(inTree,filePath,hal_id):
+    newFilename = DEFAULT_UPLOAD_FILE_NAME_PDF.format(hal_id)
+    Logger.debug('Copy original file to new one: {}->{}'.format(filePath,newFilename))
+    shutil.copyfile(filePath,newFilename)
+    # find section to add file
+    inS = inTree.find('.//editionStmt',inTree.nsmap)
+    if len(inS)==0:
+        newE = etree.Element('editionStmt',nsmap=inTree.nsmap)
+        pos= inTree.find('.//titleStmt',inTree.nsmap)
+        pos.addnext(newE)
+        inS = inTree.find('.//editionStmt',inTree.nsmap)
+    # find subsection
+    inSu = inS.find('.//edition',inTree.nsmap)
+    if len(inSu)==0:
+        inSu = etree.SubElement(inS,'edition',nsmap=inTree.nsmap)
+        
+    # check existing file
+    # nFile = inS.xpath("//ref[@type='file']") #find('.//ref',inTree.nsmap)
+    # add file
+    nFile=etree.SubElement(inSu, "ref",nsmap=inTree.nsmap)
+    nFile.set("type","file")
+    nFile.set("subtype","author")
+    nFile.set("n","1")
+    nFile.set("target",newFilename)
+    return newFilename
+
+def buildZIP(xml_file_path,pdf_file_path):
+    # create temporary directory
+    tmp_dir_path=tempfile.mkdtemp()
+    Logger.debug('Create temporary directory: {}'.format(tmp_dir_path))
+    Logger.debug('Copy XML file: {}->{}'.format(xml_file_path,tmp_dir_path))
+    shutil.copy(xml_file_path,tmp_dir_path)
+    Logger.debug('Copy PDF file: {}->{}'.format(pdf_file_path,tmp_dir_path))
+    shutil.copy(pdf_file_path,tmp_dir_path)
+    # build zip archive
+    archivePath = DEFAULT_UPLOAD_FILE_NAME_ZIP
+    Logger.debug('Create zip archive: {}'.format(archivePath+'zip'))
+    shutil.make_archive(archivePath, 'zip', tmp_dir_path)
+    return archivePath+'.zip'
+
+def preparePayload(tei_content,pdf_path,dirPath,hal_id):
+    # clean XML
+    cleanXML(tei_content)
+    # declare new file as target in xml
+    newPDF = addFileInXML(tei_content,pdf_path,hal_id)
+    # write xml file
+    xml_file_path = os.path.join(dirPath,DEFAULT_UPLOAD_FILE_NAME_XML)
+    writeXML(tei_content,xml_file_path)
+    # build zip file
+    zipfile = buildZIP(xml_file_path,newPDF)
+    #create header
+    headers = {
+        'Content-Type': 'application/zip',
+        'Export-To-Arxiv': 'false',
+        'Export-To-PMC': 'false',
+        'Packaging': 'http://purl.org/net/sword-types/AOfr',
+        'Content-Disposition': 'attachment; filename=' + xml_file_path,
+        # 'Hide-For-RePEc': 'false',
+        # 'Hide-In-OAI': 'false',
+        # 'X-Allow-Completion': 'idext,grobid,affiliation'
+        }
+    return zipfile, headers
+
+def upload2HAL(file,headers,credentials,server='preprod'):
+    Logger.info('Upload to HAL')
+    Logger.debug('File: {}'.format(file))
+    Logger.debug('Headers: {}'.format(headers))
+    
+    if server=='preprod':
+        url = ARCHIVES_SWORD_PRE_API_URL
+    else:
+        url = ARCHIVES_SWORD_API_URL
+    
+    Logger.debug('Upload via {}'.format(url))
+    # read data to sent
+    with open(file, 'rb') as f:
+        data = f.read()
+    #     res = requests.post(url=url,
+    #                     data=data,
+    
+    res = requests.post(url=url,
+                        # files={'upload.zip':file},
+                        data=data,
+                        headers=headers,
+                        auth=HTTPBasicAuth(credentials['login'],credentials['passwd']))
+
+    if res.status_code == 201:
+        Logger.info("Successfully upload to HAL.")
+    else:
+        Logger.warning('Error: {}'.format(res.text))
+        Logger.error("Failed to upload. Status code: {}".format(res.status_code))
+
+
+def extract_info(pdf_path):
+    Logger.debug('Extract title from PDF file')
+    title = titleFromPdf(pdf_path)
+    return title
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='PDF2HAL - Extract title and author from a PDF file.')
+    parser.add_argument('pdf_path', help='Path to the PDF file')
+    parser.add_argument('-c','--credentials', help='Path to the credentials file')
+    parser.add_argument('-v','--verbose', help='Show all logs',action='store_true')
+    parser.add_argument('-n','--dry-run', help='Execute on hal-preprod',action='store_true')
+    parser.add_argument('-l','--login', help='Username for API (HAL)')
+    parser.add_argument('-p','--passwd', help='Password for API (HAL)')
+    parser.add_argument('-f','--force', help='Force for no interaction',action='store_true')
+    sys.argv = ['pdf2hal.py', 'jean2012.pdf', '-v', '-f']
+    args = parser.parse_args()
+    
+    # activate verbose mode
+    if args.verbose:
+        Logger.setLevel(logging.DEBUG)
+    
+    Logger.info('Run PDF2HAL')
+    Logger.info('')
+    #
+    pdf_path = args.pdf_path
+    Logger.debug('PDF file: {}'.format(pdf_path))
+    dirPath = os.path.dirname(pdf_path)
+    Logger.debug('Directory: {}'.format(dirPath))
+    title = extract_info(pdf_path)
+    
+    # show first characters of pdf file
+    showPDFcontent(pdf_path,number=DEFAULT_NB_CHAR)
+    
+    titleOk = False
+    while not titleOk:
+        if title:
+            Logger.info(f'Title: {title}? (y/n)')
+            choice = input(' > ')
+            if choice == 'y':
+                titleOk = True
+        if not titleOk:
+            Logger.info('Provide title manually')
+            title = input(' > ')
+        
+    # Search for the PDF title in archives-ouvertes.fr
+    selected_result = dict()
+    while 'title_s' not in selected_result:
+        archives_results = search_title_in_archives(title)
+
+        if archives_results:
+            selected_result = choose_from_results(archives_results,args.force)
+            if 'title_s' not in selected_result:
+                title = selected_result
+        
+    if selected_result:
+        selected_title = selected_result.get('title_s', 'N/A')
+        selected_author = selected_result.get('author_s', 'N/A')
+        hal_id = selected_result.get('halId_s', None)
+
+        Logger.info(f'Selected result in archives-ouvertes.fr:')
+        Logger.info(f'Title: {selected_title}')
+        Logger.info(f'Author: {selected_author}')
+        Logger.info(f'HAL-id: {hal_id}')
+
+        # Download TEI file
+        tei_content = download_tei_file(hal_id)
+
+        if len(tei_content)>0:
+            # write TEI file            
+            tei_file_path = os.path.join(dirPath,hal_id+".tei.xml")
+            Logger.debug('Write TEI file: {}'.format(tei_file_path))
+            writeXML(tei_content,tei_file_path)
+            
+            #prepare payload to upload to HAL
+            file,payload=preparePayload(tei_content,pdf_path,dirPath,hal_id)
+            
+            # load credentials from file or from arguments
+            credentials = load_credentials(args)
+            
+            # upload to HAL
+            upload2HAL(file,payload,credentials)
+        
+    #     # load credentials from file or from arguments
+    #     credentials = load_credentials(args)
+
+    #     # Load credentials from file if provided
+    #     if args.credentials:
+    #         credentials = load_credentials(args.credentials)
+    #         ARCHIVES_SWORD_API_URL, ARCHIVES_SWORD_API_USERNAME, ARCHIVES_SWORD_API_PASSWORD = credentials
+
+        
+
+
+    #                 # Upload both PDF and TEI files using archives-ouvertes.fr SWORD API
+    #                 deposit_to_archives_sword_api(pdf_path, tei_content)
+    #             else:
+    #                 print("Failed to download TEI file.")
+    #         else:
+    #             print("No result selected.")
+    #     else:
+    #         print("No results found in archives-ouvertes.fr.")
+
+    # else:
+    #     print("Title or author information not found in the PDF.")
